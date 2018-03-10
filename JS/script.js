bcb93405137454e5bd53841e0af784b308a7eb6a
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-10 17:48:30 +1100

    added event listener

--- JS/script.js
+++ JS/script.js
@@ -18,3 +18,7 @@ function reveal(e) {
 // By leaving out the () on reveal ensures the funstion wont be run when it originally loads
 CTA.onclick = reveal
 //**************//
+
+// Event listener listens for an event to occur
+CTA.addEventListener("click", reveal, false)
+CTA.addEventListener("click", function(){console.log("The buton was clicked!")}, false)
