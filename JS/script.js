e8064a86153b3dc682e33e16edbb8cdc2dc5ffec
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-10 17:18:26 +1100

    added event handler

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,18 @@
 const CTA = document.querySelector(".cta a")
 const ALERT = document.querySelector("#booking-alert")
+
+CTA.classList.remove("hide")
+ALERT.classList.add("hide")
+
+
+// Toggles for when button is clicked
+function reveal() {
+    CTA.classList.toggle("hide")
+    ALERT.classList.toggle("hide")
+
+}
+//***************//
+// Event handler
+// By leaving out the () on reveal ensures the funstion wont be run when it originally loads
+CTA.onclick = reveal
+//**************//
